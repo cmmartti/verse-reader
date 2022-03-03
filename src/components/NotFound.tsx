586421d237c0c96dd5ae0de7564fec9c1e63a5b8
cmmartt <mail@charlesmarttinen.ca>
2022-03-03 17:26:03 -0500

    Update NotFound with link

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 export function NotFound() {
     return (
         <main>
             <h1>Not Found</h1>
+            <Link to="/manage">Manage Books</Link>
         </main>
     );
 }
